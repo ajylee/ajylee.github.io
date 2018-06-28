9c913c6b921471e318b0dd4ffed52086c044be72
Andrew Lee <ajylee.id.noreply@gmail.com>
2018-06-27 21:46:43 -0700

    Sort articles in make-index.rb

--- make-index.rb
+++ make-index.rb
@@ -6,10 +6,15 @@ index = Pathname('index.md')
 index_content = Pathname('index-content.md')
 
 
-articles = Pathname('.').children.select { |x|
+articles = Pathname('.')
+  .children
+  .select { |x|
   x.to_s.match(/.*\.md/) and not [index_content, index].member?(x)
-}.map { |x|
+}
+  .sort 
+  .map { |x|
   "- [#{x.to_s}](#{x.to_s})\n\n"
-}.join("\n")
+}
+  .join("\n")
 
 index.write((index_content.read + articles))
